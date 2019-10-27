d2749e5983f4833d16051f6a0a914809e8045737
xiaoyaor <747976016@qq.com>
2019-10-27 14:41:12 +0800

    上传扩展文件

---
+++ src/config/jump.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaoyaor
+ * Date: 2019/10/27
+ * Time: 14:535
+ */
+return[
+    // 默认跳转页面对应的模板文件
+    'dispatch_success_tmpl' => app()->getRootPath().'/vendor/xiaoyaor/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+    'dispatch_error_tmpl'   => app()->getRootPath().'/vendor/xiaoyaor/thinkphp-jump/src/tpl/dispatch_jump.tpl',
+];
